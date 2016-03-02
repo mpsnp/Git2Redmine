b9481d5d5c5c9a3a95587b41e7fd22ca32684ca1
George Kiriy <gkiriy@smedialink.com>
2016-03-02 15:38:02 +0300

    Initial Commit

---
+++ git2redmine/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  git2redmine
+//
+//  Created by George Kiriy on 02/03/16.
+//  Copyright © 2016 Mpsnp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
